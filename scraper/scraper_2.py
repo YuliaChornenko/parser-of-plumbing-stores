b65d5ea7c253fbc6ca24f9cc029a5fa09657d2d0
yuliachornenko <chornenko1234@gmail.com>
2020-04-08 21:36:52 +0300

    Third commit    scraper for third b2b site added    data saved in database

---
+++ scraper/scraper_2.py
@@ -0,0 +1,60 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+
+def agromat_scraper(agromat_link):
+    with urlopen(agromat_link) as r:
+        xml = r.read().decode('utf-8')
+        soup = BeautifulSoup(xml, 'xml')
+        products = soup.find_all('product')
+        agromat_dataset_list = list()
+        for prod in products:
+            Articul = prod.find('Articul').text
+            Available = prod.find('Available').text
+            Brand = prod.find('Brand').text
+            Category = prod.find('Category').text
+            Code = prod.find('Code').text
+            Collections = prod.find('Collections').text
+            Country = prod.find('Country').text
+            Group = prod.find('Group').text
+            Height = prod.find('Height').text
+            Images = prod.find('Images').text
+            Length = prod.find('Length').text
+            KgInPackage = prod.find('Length').text
+            Measure = prod.find('Measure').text
+            MetersInPackage = prod.find('MetersInPackage').text
+            Name = prod.find('Name').text
+            PackagesInPallets = prod.find('PackagesInPallets').text
+            PiecesInPackage = prod.find('PiecesInPackage').text
+            RrcPrice = prod.find('RrcPrice').text
+            Sort = prod.find('Sort').text
+            Status = prod.find('Status').text
+            Width = prod.find('Width').text
+            agromat_dataset = {
+                'Articul': Articul,
+                'Available': Available,
+                'Brand': Brand,
+                'Category': Category,
+                'Code': Code,
+                'Collections': Collections,
+                'Country': Country,
+                'Group': Group,
+                'Height': Height,
+                'Images': Images,
+                'Length': Length,
+                'KgInPackage': KgInPackage,
+                'Measure': Measure,
+                'MetersInPackage': MetersInPackage,
+                'Name': Name,
+                'PackagesInPallets': PackagesInPallets,
+                'PiecesInPackage': PiecesInPackage,
+                'RrcPrice': RrcPrice,
+                'Sort': Sort,
+                'Status': Status,
+                'Width': Width
+            }
+
+            agromat_dataset_list.append(agromat_dataset)
+
+        return agromat_dataset_list
--- scraper/site_links.py
+++ scraper/site_links.py
@@ -1,3 +1,3 @@
 sandi_link = 'https://b2b-sandi.com.ua/export_xml/2b4b03e329fa14abe9c4c89fe5b591cd'
 antey_link = 'https://b2b.antey.com.ua/index.php?r=api/GetXML&key=6d9b41ef2141bdef85e5b4440d04b0b1fe095c36af48d71d8d132c32fb50316d'
-agromat_link = 'https://partners.agromat.ua'
+agromat_link = 'https://partners.agromat.ua/profile/data/products.xml'
