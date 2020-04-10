547064143aea10a0c72cc2e04014cdb756b9cde0
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-04-10 17:51:49 +0300

    18-th commit. Test Heroku

--- tasks.py
+++ tasks.py
@@ -1,14 +1,14 @@
 from celery import Celery
 import os
+from scraper.scraper_1 import Scraper
 
 app = Celery("fr_scrapper_scheduller")
 
 @app.task
 def add(db, site):
-    print('TASK STARTER')
     sandi_collection = db['sandi_db']
-    print(222222222222222222222222222222)
     sandi_collection.delete_many({})
-    print(333333333333333333333333333333)
+    sandi_dataset_list = Scraper.get_sandi_produts(Scraper.get_soup(site))
+    sandi_collection.insert_many(sandi_dataset_list)
 
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'],CELERY_RESULT_BACKEND = os.environ['REDIS_URL'])
