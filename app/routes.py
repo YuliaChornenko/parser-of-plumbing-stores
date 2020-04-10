647305963125e341bd2c99e2e92dd507f00d7670
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-04-10 18:43:15 +0300

    19-th commit. Test Heroku

--- app/routes.py
+++ app/routes.py
@@ -47,13 +47,13 @@ def update_db_all():
     site = request.args.get('site').strip()
     if site.split('/')[2] == 'b2b-sandi.com.ua':
         sandi_update_all(db,site)
-        #prepare_csv('sandi_db')
+        prepare_csv('sandi_db')
     elif site.split('/')[2] == 'b2b.antey.com.ua':
         antey_update_all(db, site)
-        #prepare_csv('antey_db')
+        prepare_csv('antey_db')
     elif site.split('/')[2] == 'partners.agromat.ua':
         agromat_update_all(db, site)
-        #prepare_csv('agromat_db')
+        prepare_csv('agromat_db')
     else:
         return 'Проверьте корректность ссылки!'
     return 'База данных обновлена!'
--- tasks.py
+++ tasks.py
@@ -9,13 +9,19 @@ import pandas as pd
 
 app = Celery("fr_scrapper_scheduller")
 
-
-def prepare_csv(table_name):
-    client = MongoClient('mongodb://romasoya1402:Roma1989Soya@cluster0-shard-00-00-zkewx.mongodb.net:27017,cluster0-shard-00-01-zkewx.mongodb.net:27017,cluster0-shard-00-02-zkewx.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
+@app.task
+def connection(table_name):
+    client = MongoClient(
+        'mongodb://romasoya1402:Roma1989Soya@cluster0-shard-00-00-zkewx.mongodb.net:27017,cluster0-shard-00-01-zkewx.mongodb.net:27017,cluster0-shard-00-02-zkewx.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
     db = client.b2b
-    sandi_table= db[table_name].find()
+    table = db[table_name].find()
+    return table
+
+@app.task
+def prepare_csv(table_name):
+    table = connection(table_name)
     docs = pd.DataFrame(columns=[])
-    for num, doc in enumerate(sandi_table):
+    for num, doc in enumerate(table):
         doc["_id"] = str(doc["_id"])
         series_obj = pd.Series(doc)
         docs = docs.append(series_obj, ignore_index=True )
